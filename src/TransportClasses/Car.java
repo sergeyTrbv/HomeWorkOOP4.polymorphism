2646764679d4a40a0b71fa3478cc5d187ecf57d2
1 <sergey.trbv@yandex.ru>
2022-12-06 21:38:20 +0400

    Первый коммит

--- src/TransportClasses/Car.java
+++ src/TransportClasses/Car.java
@@ -2,6 +2,7 @@ package TransportClasses;
 
 public class Car extends Transport implements Competing {
 
+
     public Car(String brand, String model, double engineVolume) {
         super(brand, model, engineVolume);
     }
